99e5dce5f5d04d37a12192500467d58e69130bd8
impossiblearchitect <134196723+impossiblearchitect@users.noreply.github.com>
2025-12-04 01:36:53 -0800

    Delete obj directory

--- obj/Debug/net480/PluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace CyclopsScannerModule
-{
-    public static class PluginInfo
-    {
-        public const string PLUGIN_GUID = "org.ryuugoroshimonogatari.cyclopsscanner";
-        public const string PLUGIN_NAME = "CyclopsScannerModule";
-        public const string PLUGIN_VERSION = "1.0.0";
-    }
-}
--- obj/Release/net480/PluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace CyclopsScanner
-{
-    public static class PluginInfo
-    {
-        public const string PLUGIN_GUID = "org.ryuugoroshimonogatari.cyclopsscanner";
-        public const string PLUGIN_NAME = "CyclopsScanner";
-        public const string PLUGIN_VERSION = "1.0.0";
-    }
-}
